9f10adbfa4b7f05cbc2b1924c59736b0c243b3fc
O-Robot <adewaleogooluwani@gmail.com>
2025-04-26 18:50:14 +0100

    Update configuration files and main application structure; add device preview support and update dependencies

--- lib/zay.dart
+++ lib/zay.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:zayrova/core/themes/zay_theme.dart';
 import 'package:zayrova/presentation/routes/zay_routes.dart';
 
